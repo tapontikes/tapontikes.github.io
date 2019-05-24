369d0f59cad1a307129705b216a8ad702a633176
Thomas Pontikes <tapontikes@gmail.com>
2019-05-23 23:47:43 -0400

    timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { environment } from '../environments/environment';
+import {CountdownDate} from './model/CountdownDate';
+import * as moment from 'moment';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'After9';
+  baseUrl = environment.baseUrl;
+  openDate;
+
+
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  getTimeOpen() {
+    this.http.get(this.baseUrl + '/api/countdown').subscribe( (obj: CountdownDate) => {
+      this.openDate = moment(obj.open);
+    });
+  }
+
+  ngOnInit(): void {
+    this.getTimeOpen();
+  }
+
+
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://api-after9.duckdns.org:3000'
+
 };
